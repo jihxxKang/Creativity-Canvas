2197999288a6b99d7e6606aa84953ff65df1f11b
Jihoo Kang <73522326+jihxxKang@users.noreply.github.com>
2023-02-26 02:14:07 +0900

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,4 @@
 /*
-Node + Express Example code for CS160 Summer 2022
-Prepared by Shm Garanganao Almeda 
 
 Code referenced from: 
 https://www.digitalocean.com/community/tutorials/how-to-create-a-web-server-in-node-js-with-the-http-module"
